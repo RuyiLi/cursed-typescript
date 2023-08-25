64180a047d9d9d76f5f795360bb10897babe910d
Ruyi Li <ruyili2002@gmail.com>
2023-08-25 12:45:23 -0700

    game of life v2 (upd indices and testutil)

--- game-of-life.ts
+++ game-of-life.ts
@@ -1,183 +1,158 @@
-type Cell = "x" | ".";
+type Cell = 'x' | '.'
+type Board = Cell[][]
 
-// type EvolveRow<Row extends Cell[], PrevRow extends Cell[], NextRow extends Cell[]> =
-//   Row
+type ValidIndex<Arr extends unknown[]> =
+  Exclude<keyof Arr, Exclude<keyof Arr, `${number}`>> extends `${infer N extends number}`
+  ? N
+  : never
 
-// type Evolve<Board extends Cell[][], PrevRow extends Cell[] = []> =
-//   Board extends [infer CurrRow extends Cell[], ...infer RemainingRows extends Cell[][]]
-//   ? RemainingRows extends [infer NextRow extends Cell[], ...infer RemainingRows extends Cell[][]]
-//     ? [EvolveRow<CurrRow, PrevRow, NextRow>, ...Evolve<RemainingRows, CurrRow>]
-//     : [EvolveRow<CurrRow, PrevRow, []>]
-//   : []
-
-// expands object types one level deep
-type Expand<T> = T extends infer O ? { [K in keyof O]: O[K] } : never;
-
-// expands object types recursively
-type ExpandRecursively<T> = T extends object
-  ? T extends infer O
-    ? { [K in keyof O]: ExpandRecursively<O[K]> }
-    : never
-  : T;
-
-type Board = Cell[][];
-type ValidIndex<Arr extends Cell[] | Board> = {
-  [K in keyof Arr]: K extends `${infer N extends number}` ? N : never;
-}[keyof Arr];
+type Equals<X, Y> =
+  (<T>() => T extends X ? 1 : 2) extends
+  (<T>() => T extends Y ? 1 : 2) ? true : false;
 
 // Doesn't handle negative or decimal cases
-type Sub1<N extends number, C extends 0[] = []> = N extends 0
-  ? never
-  : N extends [...C, 0]["length"]
-  ? C["length"]
-  : Sub1<N, [...C, 0]>;
+// It's annoying to handle the never case, so I'm using -1 as a sentinel value instead
+type Sub1<N extends number, C extends 0[] = []> =
+  true extends Equals<N, 0> | Equals<N, number> 
+  ? -1
+  : N extends [...C, 0]['length']
+    ? C['length']
+    : Sub1<N, [...C, 0]>
 
-type Add1<
-  N extends Index,
-  Index extends RowIndex | ColIndex,
-  C extends 0[] = []
-> = N extends C["length"]
-  ? [...C, 0]["length"] extends Index
-    ? [...C, 0]["length"]
-    : never // prevent index overflow
-  : Add1<N, Index, [...C, 0]>;
+type Add1<N extends PossValues, PossValues extends number, C extends 0[] = []> =
+  Equals<N, number> extends true
+  ? -1
+  : N extends C['length']
+    ? [...C, 0]['length'] extends PossValues
+      ? [...C, 0]['length']
+      : -1  // prevent index out of bounds
+    : Add1<N, PossValues, [...C, 0]>
 
-type IsLive<
-  BoardState extends Board,
-  X extends ColIndex,
-  Y extends RowIndex
-> = [X] extends [never]
-  ? false
-  : [Y] extends [never]
-  ? false
-  : BoardState[Y][X] extends "x"
-  ? true
-  : false;
+type IsLive<BoardState extends Board, X extends number, Y extends number> =
+  X extends ColIndexOf<BoardState>
+  ? Y extends RowIndexOf<BoardState>
+    ? BoardState[Y][X] extends 'x'
+      ? true
+      : false
+    : false
+  : false
 
-type CountTrue<Arr extends any[], C extends 0[] = []> = Arr extends [
-  infer L,
-  ...infer R
-]
+type CountTrue<Arr extends any[], C extends 0[] = []> =
+  Arr extends [infer L, ...infer R]
   ? L extends true
     ? CountTrue<R, [...C, 0]>
     : CountTrue<R, C>
-  : C["length"];
+  : C['length']
 
-type CellState<
-  BoardState extends Board,
-  X extends ColIndex,
-  Y extends RowIndex
-> = CountTrue<
-  [
+type CellState<BoardState extends Board, X extends ColIndexOf<BoardState>, Y extends RowIndexOf<BoardState>> =
+  CountTrue<[
     IsLive<BoardState, Sub1<X>, Sub1<Y>>,
     IsLive<BoardState, X, Sub1<Y>>,
-    IsLive<BoardState, Add1<X, ColIndex>, Sub1<Y>>,
-    IsLive<BoardState, Add1<X, ColIndex>, Y>,
-    IsLive<BoardState, Add1<X, ColIndex>, Add1<Y, RowIndex>>,
-    IsLive<BoardState, X, Add1<Y, RowIndex>>,
-    IsLive<BoardState, Sub1<X>, Add1<Y, RowIndex>>,
+    IsLive<BoardState, Add1<X, ColIndexOf<BoardState>>, Sub1<Y>>,
+    IsLive<BoardState, Add1<X, ColIndexOf<BoardState>>, Y>,
+    IsLive<BoardState, Add1<X, ColIndexOf<BoardState>>, Add1<Y, RowIndexOf<BoardState>>>,
+    IsLive<BoardState, X, Add1<Y, RowIndexOf<BoardState>>>,
+    IsLive<BoardState, Sub1<X>, Add1<Y, RowIndexOf<BoardState>>>,
     IsLive<BoardState, Sub1<X>, Y>
-  ]
->;
+  ]>
 
-type EvolveCell<
-  BoardState extends Board,
-  X extends ColIndex,
-  Y extends RowIndex
-> = BoardState[Y][X] extends "."
+type EvolveCell<BoardState extends Board, X extends ColIndexOf<BoardState>, Y extends RowIndexOf<BoardState>> =
+  BoardState[Y][X] extends '.'
   ? CellState<BoardState, X, Y> extends 3
-    ? "x"
-    : "."
-  : BoardState[Y][X] extends "x"
-  ? CellState<BoardState, X, Y> extends 2 | 3
-    ? "x"
-    : "."
-  : never;
-
-// type ReplaceAt<BoardState extends Board, X extends ColIndex, Y extends RowIndex, Replacement extends Cell, CY extends 0[] = []> =
-//   CY['length'] extends Y
-//   ? [{ [K in keyof BoardState[Y]]: K extends X ? Replacement : BoardState[Y][K] }, ...BoardState]
-//   : BoardState extends [infer CurrRow, ...infer NextRows extends Board]
-//     ? [CurrRow, ReplaceAt<NextRows, X, Y,Replacement, [...CY, 0]>]
-//     : never
+    ? 'x'
+    : '.'
+  : BoardState[Y][X] extends 'x'
+    ? CellState<BoardState, X, Y> extends 2 | 3
+      ? 'x'
+      : '.'
+    : never
 
 type EvolveRow<
   OrigBoardState extends Board,
-  Y extends RowIndex,
+  Y extends RowIndexOf<OrigBoardState>,
   CX extends 0[] = [],
-  CurrRow extends Cell[] = []
-> = CX["length"] extends ColIndex
+  CurrRow extends Cell[] = [],
+> = 
+  CX['length'] extends ColIndexOf<OrigBoardState>
   ? EvolveRow<
       OrigBoardState,
       Y,
       [...CX, 0],
-      [...CurrRow, EvolveCell<OrigBoardState, CX["length"], Y>]
+      [...CurrRow, EvolveCell<OrigBoardState, CX['length'], Y>]
     >
-  : CurrRow;
+  : CurrRow
 
 type Evolve<
   OrigBoardState extends Board,
   CurrBoardState extends Board = [],
-  CY extends 0[] = []
-> = CY["length"] extends RowIndex
-  ? Evolve<
-      OrigBoardState,
-      [...CurrBoardState, EvolveRow<OrigBoardState, CY["length"]>],
-      [...CY, 0]
-    >
-  : CurrBoardState;
+  CY extends 0[] = [],
+> = 
+  CY['length'] extends RowIndexOf<OrigBoardState>
+  ? Evolve<OrigBoardState, [...CurrBoardState, EvolveRow<OrigBoardState, CY['length']>], [...CY, 0]>
+  : CurrBoardState
 
-type EvolveN<
-  BoardState extends Board,
-  N extends number,
-  C extends 0[] = []
-> = C["length"] extends N
+type EvolveN<BoardState extends Board, N extends number, C extends 0[] = []> =
+  C['length'] extends N
   ? BoardState
-  : EvolveN<Evolve<BoardState>, N, [...C, 0]>;
+  : EvolveN<Evolve<BoardState>, N, [...C, 0]>
 
-type JoinRow<Row extends Cell[], S extends string = ""> = Row extends [
-  infer L extends Cell,
-  ...infer R extends Cell[]
-]
+type RowIndexOf<BoardState extends Board> = ValidIndex<BoardState>
+type ColIndexOf<BoardState extends Board> = ValidIndex<BoardState[number]>
+
+// ================================
+// ======== BEGIN TESTING =========
+// ================================
+
+// Testing Utilities
+
+type JoinRow<Row extends Cell[], S extends string = ''> =
+  Row extends [infer L extends Cell, ...infer R extends Cell[]]
   ? JoinRow<R, `${S}${L}`>
-  : S;
+  : S
 
-type PrintBoard<
-  BoardState extends Board,
-  S extends string = ""
-> = BoardState extends [
-  infer CurrRow extends Cell[],
-  ...infer NextRows extends Board
-]
-  ? PrintBoard<NextRows, `${S}|${JoinRow<CurrRow>}`>
-  : S;
+type PrintBoard<BoardState extends Board, LineSep extends string = '|', S extends string = ''> = 
+  BoardState extends [infer CurrRow extends Cell[], ...infer NextRows extends Board]
+  ? PrintBoard<NextRows, LineSep, `${S}${LineSep}${JoinRow<CurrRow>}`>
+  : S
 
-// doesnt work when i put this logic in validindex for some reason
-type RowIndex<G = ValidIndex<Board1>> = G extends number
-  ? Board1[G] extends Cell[]
-    ? G
-    : never
-  : never;
-type ColIndex<G = ValidIndex<Board1[0]>> = G extends number
-  ? Board1[0][G] extends Cell
-    ? G
-    : never
-  : never;
+type StringToBoard<S extends string, CurrBoard extends Board = [], CurrRow extends Cell[] = []> =
+  S extends `${infer L}${infer R}`
+  ? L extends '\n'
+    ? CurrRow['length'] extends 0
+      ? StringToBoard<R, CurrBoard, []>
+      : StringToBoard<R, [...CurrBoard, CurrRow], []>
+    : L extends Cell
+      ? StringToBoard<R, CurrBoard, [...CurrRow, L]>
+      : StringToBoard<R, CurrBoard, CurrRow>
+  : CurrBoard
 
-// prettier-ignore
-type Board1 = [
-  ['.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.'],
-  ['.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', 'x', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.'],
-  ['.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', 'x', '.', 'x', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.'],
-  ['.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', 'x', 'x', '.', '.', '.', '.', '.', '.', 'x', 'x', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', 'x', 'x', '.'],
-  ['.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', 'x', '.', '.', '.', 'x', '.', '.', '.', '.', 'x', 'x', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', 'x', 'x', '.'],
-  ['.', 'x', 'x', '.', '.', '.', '.', '.', '.', '.', '.', 'x', '.', '.', '.', '.', '.', 'x', '.', '.', '.', 'x', 'x', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.'],
-  ['.', 'x', 'x', '.', '.', '.', '.', '.', '.', '.', '.', 'x', '.', '.', '.', 'x', '.', 'x', 'x', '.', '.', '.', '.', 'x', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.'],
-  ['.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', 'x', '.', '.', '.', '.', '.', 'x', '.', '.', '.', '.', '.', '.', '.', 'x', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.'],
-  ['.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', 'x', '.', '.', '.', 'x', '.', '.', '.', '.', '.', '.', '.', '.', 'x', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.'],
-  ['.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', 'x', 'x', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.'],
-  ['.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.'],
-]
+// Test Cases
+// Hover over the type names to see the output
 
-type PBoard1 = PrintBoard<Board1>;
-type PBoard2 = PrintBoard<EvolveN<Board1, 30>>;
+type Blinker = StringToBoard<`
+...
+xxx
+...
+`>
+
+type PBlinker1 = PrintBoard<Blinker>
+type PBlinker2 = PrintBoard<Evolve<Blinker>>
+type PBlinker3 = PrintBoard<EvolveN<Blinker, 2>>
+type PBlinker4 = PrintBoard<EvolveN<Blinker, 3>>
+
+type GosperGliderGun = StringToBoard<`
+........................................
+.........................x..............
+.......................x.x..............
+.............xx......xx..............xx.
+............x...x....xx..............xx.
+.xx........x.....x...xx.................
+.xx........x...x.xx....x................
+...........x.....x.......x..............
+............x...x........x..............
+.............xx.........................
+........................................
+`>
+
+type PGun1 = PrintBoard<GosperGliderGun>
+type PGun2 = PrintBoard<EvolveN<GosperGliderGun, 30>>
