8449f65b40f2bbc6061846c5f0a22dea0455e261
Ruyi Li <ruyili2002@gmail.com>
2024-02-10 17:39:40 -0500

    print formatting and some notes

--- random/sudoku.ts
+++ random/sudoku.ts
@@ -82,6 +82,8 @@ type ReplaceCell<
 // !! A temporary (bad) fix is to locate this line in the tsserver file:
 // !! `if (instantiationDepth === 100 || instantiationCount >= 5e6) {`
 // !! and remove the instantiationCount limit.
+// !! TODO: use custom ds/strings instead of arrays 
+// !!       https://herringtondarkholme.github.io/2023/04/30/typescript-magic/
 // false = unsolvable, never = should actually never happen
 type Solve<BoardState extends Board, C extends 0[] = [0]> =
   // if no more zeroes
@@ -94,6 +96,7 @@ type Solve<BoardState extends Board, C extends 0[] = [0]> =
         // go next, can't use current cell value
         ? Solve<BoardState, [...C, 0]>
         // cache result to prevent exponential growth
+        // TODO verify: actually, ts might already cache it for us?
         : Solve<ReplaceCell<BoardState, X, Y, C['length']>> extends infer Result
           ? Result extends false
             // go next, current cell value didn't work
@@ -121,17 +124,28 @@ type StringToBoard<S extends string, CurrBoard extends Board = [], CurrRow exten
       : StringToBoard<R, CurrBoard, CurrRow>
   : CurrBoard
 
-type JoinRow<Row extends Cell[], S extends string = ''> =
+type JoinRow<Row extends Cell[], S extends string = '', C extends 0[] = []> =
   Row extends [infer L extends Cell, ...infer R extends Cell[]]
-  ? JoinRow<R, `${S}${L}`>
+  ? S extends ''
+    ? JoinRow<R, `${L}`, [0]>
+    : C['length'] extends 3 | 6
+      ? JoinRow<R, `${S} | ${L}`, [...C, 0]>
+      : JoinRow<R, `${S} ${L}`, [...C, 0]>
   : S
 
-// TODO better formatting
-type PrintBoard<BoardState extends Board, LineSep extends string = '\n', S extends string = ''> = 
+type PrintBoard<
+  BoardState extends Board,
+  LineSep extends string = '\n',
+  BoxSep extends string = '------+-------+------',
+  S extends string = '',
+  C extends 0[] = []
+> = 
   BoardState extends [infer CurrRow extends Cell[], ...infer NextRows extends Board]
   ? S extends ''
-    ? PrintBoard<NextRows, LineSep, JoinRow<CurrRow>>
-    : PrintBoard<NextRows, LineSep, `${S}${LineSep}${JoinRow<CurrRow>}`>
+    ? PrintBoard<NextRows, LineSep, BoxSep, JoinRow<CurrRow>, [0]>
+    : C['length'] extends 3 | 6
+      ? PrintBoard<NextRows, LineSep, BoxSep, `${S}${LineSep}${BoxSep}${LineSep}${JoinRow<CurrRow>}`, [...C, 0]>
+      : PrintBoard<NextRows, LineSep, BoxSep, `${S}${LineSep}${JoinRow<CurrRow>}`, [...C, 0]>
   : S
 
 // Hover over this to see the solution
@@ -166,37 +180,38 @@ type SudokuEasy = StringToBoard<`
 //   0 0 0  0 8 0  0 7 9
 // `>
 
-type SudokuHardSolved = PrintBoard<Solve<SudokuHard>>
-type SudokuHard = StringToBoard<`
-  0 8 0  0 9 0  0 0 0
-  0 0 6  7 1 0  0 0 5
-  0 3 0  0 4 6  0 9 0
+// type SudokuHardSolved = PrintBoard<Solve<SudokuHard>>
+// type SudokuHard = StringToBoard<`
+//   0 8 0  0 9 0  0 0 0
+//   0 0 6  7 1 0  0 0 5
+//   0 3 0  0 4 6  0 9 0
   
-  7 0 2  1 5 0  6 0 0
-  6 0 0  0 0 0  1 0 0
-  0 0 0  0 0 0  9 5 0
+//   7 0 2  1 5 0  6 0 0
+//   6 0 0  0 0 0  1 0 0
+//   0 0 0  0 0 0  9 5 0
 
-  2 0 8  0 0 0  0 0 0
-  0 0 0  0 2 0  7 0 0
-  0 1 0  6 7 0  0 3 2
-`>
+//   2 0 8  0 0 0  0 0 0
+//   0 0 0  0 2 0  7 0 0
+//   0 1 0  6 7 0  0 3 2
+// `>
 
-// Format: NYT Difficulty MM DD YY
+// // Format: NYT Difficulty MM DD YY
 
-type NYTEasy021024 = PrintBoard<Solve<StringToBoard<`
-  4 0 1  0 5 0  9 0 0
-  3 0 0  0 6 0  7 0 4
-  7 9 0  8 0 0  0 1 0
+// type NYTEasy021024 = PrintBoard<Solve<StringToBoard<`
+//   4 0 1  0 5 0  9 0 0
+//   3 0 0  0 6 0  7 0 4
+//   7 9 0  8 0 0  0 1 0
 
-  1 0 3  7 0 0  0 0 6
-  0 8 5  0 0 4  0 0 2
-  0 0 0  6 0 0  0 8 1
+//   1 0 3  7 0 0  0 0 6
+//   0 8 5  0 0 4  0 0 2
+//   0 0 0  6 0 0  0 8 1
 
-  8 3 4  0 7 2  0 0 0
-  0 0 7  4 8 6  2 0 0
-  0 2 0  0 0 1  8 4 7
-`>>>
+//   8 3 4  0 7 2  0 0 0
+//   0 0 7  4 8 6  2 0 0
+//   0 2 0  0 0 1  8 4 7
+// `>>>
 
+// Hits instantiation count limit
 // type NYTMedium021024 = PrintBoard<Solve<StringToBoard<`
 //   0 0 5  0 0 0  3 0 8
 //   7 0 0  0 0 0  0 0 0
